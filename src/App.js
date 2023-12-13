ed422c110e9e076881f016c672c83b5f71839b49
Rhys Gregory <88214579+itsrhys754@users.noreply.github.com>
2023-12-13 13:38:55 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-// App.js
-import React from 'react';
+// components/App.js
+import React, { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
-import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 import DashboardContent from './components/DashboardContent';
+import Connections from './components/Connections';
+import Login from './components/Login'; // Import the Login component
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to set the user as logged in
+  const handleLogin = () => {
+    // Add your login logic here
+    // For now, let's simulate a successful login
+    setIsLoggedIn(true);
+  };
+
+  // Function to set the user as logged out
+  const handleLogout = () => {
+    // Add your logout logic here
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div style={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppHeader />
-      <Sidebar />
-      <Container maxWidth="xl" style={{ marginTop: 64, marginLeft: 240, padding: 20, position: 'relative', zIndex: 1 }}>
-        <DashboardContent />
-      </Container>
-    </div>
+    <Router>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <CssBaseline />
+        <AppHeader
+          isLoggedIn={isLoggedIn}
+          onLogin={handleLogin}
+          onLogout={handleLogout}
+        />
+        <div style={{ display: 'flex', flex: 1 }}>
+          {/* Conditionally render Sidebar based on login status */}
+          {isLoggedIn}
+          <Container
+          maxWidth="xl"
+          style={{ marginTop: 64, padding: 20, position: 'relative', zIndex: 1 }}
+        >
+            <Routes>
+              <Route
+                path="/"
+                element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />}
+              />
+              {isLoggedIn && (
+                <Route path="/dashboard" element={<DashboardContent />} />
+              )}
+              <Route path="/connections" element={<Connections />} />
+              <Route
+                path="/login"
+                element={<Login isLoggedIn={isLoggedIn} onLogin={handleLogin} />}
+              />
+            </Routes>
+          </Container>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
