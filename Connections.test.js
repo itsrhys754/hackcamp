bad024a998ffce1742d7e8af61a255547894e035
Rhys Gregory <88214579+itsrhys754@users.noreply.github.com>
2023-12-13 12:54:35 +0000

    Delete Connections.test.js

--- Connections.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Connections from './components/Connections';
-
-test('renders learn react link', () => {
-  render(<Connections />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
